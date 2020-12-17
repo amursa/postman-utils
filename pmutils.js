d4a28a3238052ac71f83462fca8c880cf9210549
Alexandru Mursa <alex.mursa@shopkick.com>
2020-12-17 11:06:14 -0500

    Adding setOntologyParams

--- pmutils.js
+++ pmutils.js
@@ -1,3 +1,34 @@
+//modules
+const moment = require('moment');
+var _ = require('lodash');
+var uuid = require('uuid');
+
+var initialTenantId = pm.globals.get("tenantId");
+var initialUserEmail = pm.globals.get("userEmail");
+// pm.globals.clear();
+pm.environment.clear();
+pm.collectionVariables.clear();
+pm.environment.set("tenantId", initialTenantId);
+pm.environment.set("userEmail", initialUserEmail);
+
+//Set debug to true to enable debug messages throughout the code
+if (!pm.environment.get("debug")) {
+    pm.environment.set("debug", false);
+}
+var debug = pm.environment.get("debug");
+
+//Endpoints
+postman.setEnvironmentVariable("campaignManagementServiceBaseUrl", "http://localhost:9096/campaign/v1");
+postman.setEnvironmentVariable("adminPortalServingServiceBaseUrl", "http://localhost:9098/adminBackend");
+postman.setEnvironmentVariable("orderManagementServiceBaseUrl", "http://localhost:9094/order/v1");
+postman.setEnvironmentVariable("companyManagementServiceBaseUrl", "http://localhost:9090/company/v1");
+postman.setEnvironmentVariable("triggerManagementServiceUrl", "localhost:9103/trigger/management/api/v1/triggers");
+postman.setEnvironmentVariable("engagementsBU", "http://localhost:9095/engagement/v1");
+postman.setEnvironmentVariable("triggerExecutorServiceUrl", "localhost:9101/trigger/executor/api/v1/triggers");
+postman.setEnvironmentVariable("adServingServiceBaseUrl", "localhost:9100/ad/v1");
+
+
+//Functions
 postman.setEnvironmentVariable("setHeader", () => {
     pm.request.headers.add({
         key: 'X-Tenant-Id',
@@ -27,8 +58,44 @@ postman.setEnvironmentVariable("setHeader", () => {
         })
     ]
     pm.environment.set('listHeader', listHeader);
-
 });
 setHeader = eval(pm.environment.get("setHeader"));
 setHeader();
+
+postman.setEnvironmentVariable("setOntologyParams", () => {
+    let options = {
+    url: pm.environment.get("adminPortalServingServiceBaseUrl") + "/legacy/v1/ontology/taxonomyNodes",
+    method: 'GET',
+    header: pm.environment.get('listHeader'),
+    };
+
+    pm.sendRequest(options, function (err, response) {
+        var jsonData = response.json();
+            // toPrint("First Ontology Node is " + jsonData[0].node_id + " and length of the array is " + jsonData.length, debug, true);
+            var ontologyIds = [];
+            var ontologyKeywords = [];
+            _.each(jsonData, (item) => {
+                //console.log("Item is " + item);
+                ontologyIds.push(item.node.node_id);
+            });
+            toPrint("List of ontologyIds is " + ontologyIds, debug, true);
+            var ontologyNodeId =  _.sample(ontologyIds);
+            toPrint("Choosen ontologyNodeId " + ontologyNodeId, true, true);
+            pm.collectionVariables.set("ontologyNodeId", JSON.stringify(ontologyNodeId));
+
+
+            _.each(jsonData, (item) => {
+                if (item.node.node_id == ontologyNodeId) {
+                _.each(item.childNodes, (childitem) => {
+                    ontologyKeywords.push(childitem.node_name);
+                });
+                toPrint("Choosen ontologyKeywords " + ontologyKeywords, true, true);
+                pm.collectionVariables.set("ontologyKeywords", JSON.stringify(ontologyKeywords));
+            }
+        });
+    });
+});
+setOntologyParams = eval(pm.environment.get("setOntologyParams"));
+setOntologyParams();
+
 
