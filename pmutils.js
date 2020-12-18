7431bf8fa2ceafd1f00368db8a968f41178923a3
Alexandru Mursa <alex.mursa@shopkick.com>
2020-12-18 17:37:06 -0500

    Adding setOntologyParams

--- pmutils.js
+++ pmutils.js
@@ -12,14 +12,13 @@ postman.setEnvironmentVariable("setup", () => {
     pm.collectionVariables.clear();
     pm.environment.set("tenantId", initialTenantId);
     pm.environment.set("userEmail", initialUserEmail);
-
-    
-    if (pm.environment.has("debug"))
-        var debug = pm.environment.get("debug");
-    else {
-        pm.environment.set("debug", false);
-    }
-
+    pm.environment.has("debug") ? var debug = pm.environment.get("debug"): pm.environment.set("debug", false);
+    pm.environment.has("barcodes") ? pm.environment.set("barcodes", "[{\"barcode\": \"62600963840\",\"enabled\": true, \"isUPC\": true }]");
+    pm.environment.has("chainId") ? pm.environment.set("chainId", JSON.stringify({"661250086":true}));
+    pm.environment.has("location") ? pm.environment.set("location", JSON.stringify({"14803720197":true}));
+    // pm.environment.set("barcodes", "[{\"barcode\": \"62600963840\",\"enabled\": true, \"isUPC\": true }]");
+    // pm.environment.set("chainId", JSON.stringify({"661250086":true}));
+    // pm.environment.set("location", JSON.stringify({"14803720197":true}));
     //Endpoints
     postman.setEnvironmentVariable("campaignManagementServiceBaseUrl", "http://localhost:9096/campaign/v1");
     postman.setEnvironmentVariable("adminPortalServingServiceBaseUrl", "http://localhost:9098/adminBackend");
@@ -78,7 +77,7 @@ postman.setEnvironmentVariable("setOntologyParams", () => {
                 //console.log("Item is " + item);
                 ontologyIds.push(item.node.node_id);
             });
-            // toPrint("List of ontologyIds is " + ontologyIds, debug, true);
+            toPrint("List of ontologyIds is " + ontologyIds, debug, true);
             var ontologyNodeId =  _.sample(ontologyIds);
             // toPrint("Choosen ontologyNodeId " + ontologyNodeId, true, true);
             pm.collectionVariables.set("ontologyNodeId", JSON.stringify(ontologyNodeId));
@@ -89,43 +88,11 @@ postman.setEnvironmentVariable("setOntologyParams", () => {
                 _.each(item.childNodes, (childitem) => {
                     ontologyKeywords.push(childitem.node_name);
                 });
-                // toPrint("Choosen ontologyKeywords " + ontologyKeywords, true, true);
+                toPrint("Choosen ontologyKeywords " + ontologyKeywords, true, true);
                 pm.collectionVariables.set("ontologyKeywords", JSON.stringify(ontologyKeywords));
             }
         });
     });
-});
-
-postman.setEnvironmentVariable("setup", () => {
-    //modules
-    const moment = require('moment');
-    var _ = require('lodash');
-    var uuid = require('uuid');
-
-    var initialTenantId = pm.globals.get("tenantId");
-    var initialUserEmail = pm.globals.get("userEmail");
-    // pm.globals.clear();
-    pm.environment.clear();
-    pm.collectionVariables.clear();
-    pm.environment.set("tenantId", initialTenantId);
-    pm.environment.set("userEmail", initialUserEmail);
-
-    
-    // if (pm.environment.has("debug"))
-    //     var debug = pm.environment.get("debug");
-    // else {
-    //     pm.environment.set("debug", false);
-    // }
-
-    //Endpoints
-    postman.setEnvironmentVariable("campaignManagementServiceBaseUrl", "http://localhost:9096/campaign/v1");
-    postman.setEnvironmentVariable("adminPortalServingServiceBaseUrl", "http://localhost:9098/adminBackend");
-    postman.setEnvironmentVariable("orderManagementServiceBaseUrl", "http://localhost:9094/order/v1");
-    postman.setEnvironmentVariable("companyManagementServiceBaseUrl", "http://localhost:9090/company/v1");
-    postman.setEnvironmentVariable("triggerManagementServiceUrl", "localhost:9103/trigger/management/api/v1/triggers");
-    postman.setEnvironmentVariable("engagementsBU", "http://localhost:9095/engagement/v1");
-    postman.setEnvironmentVariable("triggerExecutorServiceUrl", "localhost:9101/trigger/executor/api/v1/triggers");
-    postman.setEnvironmentVariable("adServingServiceBaseUrl", "localhost:9100/ad/v1");
 });
 
 postman.setEnvironmentVariable("toPrint", (logMessage, debug, localDebug) => {
